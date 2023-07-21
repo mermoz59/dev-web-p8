ebfc389e17f478bf6a846d715cfda2cec56ea0c6
Amine <amine.touba59@gmail.com>
2023-07-21 20:30:57 +0200

    add build-prod

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/dev-web-p8/',
-  plugins: [react()],
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/dev-web-p8/',
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+
+  console.log('Vite running in mode ${mode}')
+
+  return {
+    base: mode === 'prod' ? '' : '/dev-web-p8/',
+    plugins: [react()]
+  }
 })
